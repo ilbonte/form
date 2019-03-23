8a49987229e7041276a754a3329c3ca8bbc71438
Davide Bontempelli <davide.bonte@gmail.com>
2019-03-23 22:22:46 +0100

    try to add no-ssr

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,28 +1,30 @@
 <template>
-  <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>
-        Your Name:
-        <input type="text" name="name">
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Email:
-        <input type="email" name="email">
-      </label>
-    </p>
+  <no-ssr>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name:
+          <input type="text" name="name">
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:
+          <input type="email" name="email">
+        </label>
+      </p>
 
-    <p>
-      <label>
-        Message:
-        <textarea name="message"></textarea>
-      </label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+      <p>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </no-ssr>
 </template>
 
 <script>
