f012a87c4cbd2e6c57fcdef002d2fc5628266f93
Davide Bontempelli <davide.bonte@gmail.com>
2019-03-23 22:08:13 +0100

    add no-netifly form

---
+++ pages/plain.vue
@@ -0,0 +1,33 @@
+<template>
+  <form name="plain" method="POST">
+    <p>
+      <label>
+        Your Name:
+        <input type="text" name="name">
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Email:
+        <input type="email" name="email">
+      </label>
+    </p>
+
+    <p>
+      <label>
+        Message:
+        <textarea name="message"></textarea>
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
